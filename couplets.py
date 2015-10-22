ac23692a0fcb1b73a20bb10f47f87f2d3f537aa1
nathaniel smith <nathanielksmith@gmail.com>
2015-10-22 12:59:49 -0700

    do not rhyme words with themselves

--- couplets.py
+++ couplets.py
@@ -11,21 +11,26 @@ tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 # For looking up word sounds:
 cmudict = nltk.corpus.cmudict.dict()
 
+def get_last_word(sentence):
+    """Given a sentence, return its last word, accounting for punctuation."""
+    words = sentence.split(' ')
+    last_word = words[-1].rstrip() # strip any whitespace at the end
+    # Leaving punctuation in will make it impossible to look up words
+    for punctuation in ['!', '.', '?']:
+        last_word = last_word.replace(punctuation, '')
+
+    return last_word.lower()
+
 def rhyme_sound(sentence):
     """Given a sentence, this function returns a string representing the rhyme
     sound of the last word of the sentence. If it can't figure out the sound,
     it returns None."""
 
-    words = sentence.split(' ')
-    last_word = words[-1].rstrip() # strip any whitespace at the end
-
-    # Leaving punctuation in will make it impossible to look up words
-    for punctuation in ['!', '.', '?']:
-        last_word = last_word.replace(punctuation, '')
+    last_word = get_last_word(sentence)
 
     # Always take the first pronounciation option that cmudict gives. If
     # cmudict cannot find the pronounciation, have us get None back instead.
-    phonemes = cmudict.get(last_word.lower(), [None])[0]
+    phonemes = cmudict.get(last_word, [None])[0]
 
     # Give up if we found no pronounciation.
     if phonemes is None:
@@ -35,39 +40,42 @@ def rhyme_sound(sentence):
     # last word:
     return ''.join(phonemes[-3:])
 
-# Read our book
-f = open('paradise_lost.txt')
-text = ''.join(f.readlines())
-f.close()
+if __name__ == '__main__':
+    # Read our book
+    f = open('paradise_lost.txt')
+    text = ''.join(f.readlines())
+    f.close()
 
-# Extract sentences
-sentences = tokenizer.tokenize(text)
+    # Extract sentences
+    sentences = tokenizer.tokenize(text)
 
-# Create a corresponding list of rhyme sounds
-rhyme_sounds = list(map(rhyme_sound, sentences))
+    # Create a corresponding list of rhyme sounds
+    rhyme_sounds = list(map(rhyme_sound, sentences))
 
-couplets = []
+    couplets = []
 
-# Now we'll go looking for couplets.
-for counter in range(0, len(rhyme_sounds)):
-    # Clean up any new lines still in the original text:
-    sentence = sentences[counter].replace('\n', '')
+    # Now we'll go looking for couplets.
+    for counter in range(0, len(rhyme_sounds)):
+        # Clean up any new lines still in the original text:
+        sentence = sentences[counter].replace('\n', '')
 
-    rhyme_sound = rhyme_sounds[counter]
+        rhyme_sound = rhyme_sounds[counter]
 
-    if rhyme_sound is not None:
-        # enumerate() takes a list and returns a new list of pairs like [0, "a"], [1, "b"]
-        # We want all the indices in rhyme_sounds that correspond to the sound
-        # we're looking for, except for the index we are currently on.
-        indices = [i for i, x in enumerate(rhyme_sounds) if x == rhyme_sound and i != counter]
-        for index in indices:
-            # Clean up any new lines still in the original text:
-            rhyming_sentence = sentences[index].replace('\n', '')
+        if rhyme_sound is not None:
+            # enumerate() takes a list and returns a new list of pairs like [0, "a"], [1, "b"]
+            # We want all the indices in rhyme_sounds that correspond to the sound
+            # we're looking for, except for the index we are currently on.
+            indices = [i for i, x in enumerate(rhyme_sounds) if x == rhyme_sound and i != counter]
+            for index in indices:
+                # Clean up any new lines still in the original text:
+                rhyming_sentence = sentences[index].replace('\n', '')
 
-            # Add the sentence we're working on and its rhyme to our list of couplets:
-            couplets.append([sentence, rhyming_sentence])
+                # Add the sentence we're working on and its rhyme to our list of couplets,
+                # but only if the rhyming word is not the same.
+                if get_last_word(sentence) != get_last_word(rhyming_sentence):
+                    couplets.append([sentence, rhyming_sentence])
 
-for couplet in couplets:
-    print(couplet[0])
-    print("\t", couplet[1])
-    print('')
+                for couplet in couplets:
+                    print(couplet[0])
+                    print("\t", couplet[1])
+                    print('')
